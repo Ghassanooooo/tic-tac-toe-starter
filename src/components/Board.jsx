f746749b350d731725354bbbe2a853871a3d08f8
Ghassanooooo <35297725+Ghassanooooo@users.noreply.github.com>
2018-08-15 14:44:15 +0200

    made the s daynamic

---
+++ src/components/Board.jsx
@@ -0,0 +1,40 @@
+import React, {Component, Fragment} from 'react';
+
+import Square from './Square';
+
+class Board extends Component {
+  state = {
+    squares:['X','X','X','X','X','X','X','X','X']
+  }
+  renderSquare=(i)=> {
+   const updateState = {
+      ...this.state,
+      squares: [...this.state.squares]
+    }
+  
+    updateState.squares[i] === 'X' ?
+       updateState.squares[i] = 'O'
+        :
+       updateState.squares[i] = 'X'
+   
+
+    this.setState({squares: updateState.squares})
+  }
+
+  render() {
+    const status = 'Next player: X';
+    const outputSquares = this.state.squares.map((s,index)=>{
+      return <Square key={index} value={s} clicked={()=>this.renderSquare(index)} />
+    })
+    return (
+      <Fragment>
+        <div className="status h2 text-center">{status}</div>
+        <div className="board">
+          {outputSquares}
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default Board;
