016054c5db4beae0247dee8ff0874ac42d151533
Ghassanooooo <35297725+Ghassanooooo@users.noreply.github.com>
2018-08-15 14:59:47 +0200

    init the s value with null

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,30 +1,31 @@
-import React, {Component, Fragment} from 'react';
+import React, { Component, Fragment } from 'react';
 
 import Square from './Square';
 
 class Board extends Component {
   state = {
-    squares:['X','X','X','X','X','X','X','X','X']
+    squares: [null, null, null, null, null, null, null, null, null]
   }
-  renderSquare=(i)=> {
-   const updateState = {
+  renderSquare = (i) => {
+    const updateState = {
       ...this.state,
       squares: [...this.state.squares]
     }
-  
-    updateState.squares[i] === 'X' ?
-       updateState.squares[i] = 'O'
-        :
-       updateState.squares[i] = 'X'
-   
 
-    this.setState({squares: updateState.squares})
+    updateState.squares[i] === null  ?
+      updateState.squares[i] ='X'   :
+      updateState.squares[i] === 'X' ?
+        updateState.squares[i] = 'O'  :
+        updateState.squares[i] = 'X'
+
+
+    this.setState({ squares: updateState.squares })
   }
 
   render() {
     const status = 'Next player: X';
-    const outputSquares = this.state.squares.map((s,index)=>{
-      return <Square key={index} value={s} clicked={()=>this.renderSquare(index)} />
+    const outputSquares = this.state.squares.map((s, index) => {
+      return <Square key={index} value={s} clicked={() => this.renderSquare(index)} />
     })
     return (
       <Fragment>
